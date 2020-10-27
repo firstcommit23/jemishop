6c29d6f21e35392f1f1b39362012ea1424581a50
jm131313 <jm131313.kim@gmail.com>
2020-10-27 22:43:09 +0900

    정리

--- server.js
+++ server.js
@@ -6,8 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/item', (req, res) => {
+    res.send([
+        {
+          id : 0,
+          title : "White and Black",
+          content : "Born in France",
+          price : 120000
+        },
+      
+        {
+          id : 1,
+          title : "Red Knit",
+          content : "Born in Seoul",
+          price : 110000
+        },
+      
+        {
+          id : 2,
+          title : "Grey Yordan",
+          content : "Born in the States",
+          price : 130000
+        }
+      ]);
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
